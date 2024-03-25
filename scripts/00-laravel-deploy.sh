331c1985890b742fd6cce675bbbc1190c168c9ca
seasoned-coder <kevin.peirce@btinternet.com>
2024-03-25 21:14:41 +0000

    added swagger

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,3 +11,7 @@ php artisan route:cache
 
 echo "Running migrations and seeding..."
 php artisan migrate:fresh --force --seed
+
+echo "Doing swagger...."
+php artisan vendor:publish --provider "L5Swagger\L5SwaggerServiceProvider"
+php artisan l5-swagger:generate
